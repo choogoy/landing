feec4ea6688205b29e1377facf63d695303861a6
choogoy <victormm0312@yandex.ru>
2021-04-22 17:43:18 +0300

    lesson19 обязательное задание

--- script/script.js
+++ script/script.js
@@ -46,6 +46,108 @@ window.addEventListener('DOMContentLoaded', () => {
 
   };
 
-  setInterval(countTimer, 1000, '22 April 2021');
+  setInterval(countTimer, 1000, '23 April 2021');
+
+  // Menu
+  const toggleMenu = () => {
+    const btnMenu = document.querySelector('.menu'),
+      menu = document.querySelector('menu'),
+      closeBtn = document.querySelector('.close-btn'),
+      menuItems = menu.querySelectorAll('ul>li');
+
+    const handlerMenu = () => {
+      menu.classList.toggle('active-menu');
+    };
+
+    btnMenu.addEventListener('click', handlerMenu);
+    closeBtn.addEventListener('click', handlerMenu);
+    menuItems.forEach(elem => elem.addEventListener('click', handlerMenu));
+
+  };
+
+  toggleMenu();
+
+  const animate = ({timing, draw, duration}) => {
+
+    let start = performance.now();
+  
+    requestAnimationFrame(function animate(time) {
+      // timeFraction изменяется от 0 до 1
+      let timeFraction = (time - start) / duration;
+      
+      if (timeFraction > 1) {
+        timeFraction = 1;
+      }
+  
+      // вычисление текущего состояния анимации
+      let progress = timing(timeFraction);
+  
+      draw(progress); // отрисовать её
+  
+      if (timeFraction < 1) {
+        requestAnimationFrame(animate);
+      }
+  
+    });
+  };
+
+  // Popup
+  const togglePopup = () => {
+    const popup = document.querySelector('.popup'),
+      popupBtns = document.querySelectorAll('.popup-btn'),
+      popupClose = document.querySelector('.popup-close');
+
+    const openModal = () => {
+
+      popup.style.display = 'block';
+      popup.style.opacity = 0;
+
+      if (window.innerWidth >= 768) {
+        
+        animate({
+          duration: 1000,
+          timing(timeFraction) {
+            return timeFraction;
+          },
+          draw(progress) {
+            popup.style.opacity = progress;
+          }
+        });
+
+      } else {
+        popup.style.opacity = 1;
+      }
+
+    };
+  
+    const closeModal = () => {
+
+      if (window.innerWidth >= 768) {
+
+        animate({
+          duration: 500,
+          timing(timeFraction) {
+            return timeFraction;
+          },
+          draw(progress) {
+            popup.style.opacity = 1 - progress;
+            setTimeout(() => {
+              popup.style.display = 'none';
+            }, 500);
+          }
+        });
+
+      } else {
+        popup.style.display = 'none';
+        popup.style.opacity = 0;
+      }
+
+    };
+
+    popupBtns.forEach(btn => btn.addEventListener('click', openModal));
+    popupClose.addEventListener('click', closeModal);
+  };
+
+  togglePopup();
 
 });
