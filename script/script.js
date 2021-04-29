e4420d3938ac73ba943a426caf6636002dc2dde7
choogoy <victormm0312@yandex.ru>
2021-04-29 07:46:31 +0300

    lesson24 обязательное задание

--- script/script.js
+++ script/script.js
@@ -349,18 +349,61 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Calculator
 
-  const validCalc = () => {
-    const calcBlock = document.querySelector('.calc-block');
+  const calc = (price = 100) => {
 
-    calcBlock.addEventListener('input', event => {
-      const input = event.target.closest('input');
-      if (input) {
-        input.value = input.value.replace(/\D/gi, '').replace(/^0/gi, ''); // только цифры и первая не ноль 
+    const calcBlock = document.querySelector('.calc-block');
+    const calcType = document.querySelector('.calc-type');
+    const calcSquare = document.querySelector('.calc-square');
+    const calcCount = document.querySelector('.calc-count');
+    const calcDay = document.querySelector('.calc-day');
+    const totalValue = document.getElementById('total');
+
+    const countSum = () => {
+      let total = 0;
+      let countValue = 1;
+      let dayValue = 1;
+      const squareValue = +calcSquare.value;
+      const typeValue = calcType.options[calcType.selectedIndex].value;
+
+      if (calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+      }
+
+      if (calcDay.value && calcDay.value < 5) {
+        dayValue *= 2;
+      } else if (calcDay.value && calcDay.value < 10) {
+        dayValue *= 1.5;
+      }
+
+      if (typeValue && squareValue) {
+        console.log(typeValue, squareValue);
+        total = price * typeValue * squareValue * countValue * dayValue;
+      }
+
+      totalValue.textContent = total;
+    };
+
+    calcBlock.addEventListener('change', event => {
+      const target = event.target;
+
+      if (target.matches('select') || target.matches('input')) {
+        countSum();
       }
     });
+
+    const validCalc = () => {
+      calcBlock.addEventListener('input', event => {
+        const input = event.target.closest('input');
+        if (input) {
+          input.value = input.value.replace(/\D/gi, '').replace(/^0/gi, ''); // только цифры и первая не ноль 
+        }
+      });
+    };
+  
+    validCalc();
   };
 
-  validCalc();
+  calc(100);
 
   // Footer form validator
  
